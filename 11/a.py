3d5fc14d848c3eed227940ab78edb8e7ccb9a79f
Håvard Moe Jacobsen <havardmj@hotmail.com>
2023-12-11 20:13:05 +0100

    11

--- 11/a.py
+++ 11/a.py
@@ -1,10 +1,24 @@
 import re
 import numpy as np
 
+def fill_empty_space(space):
+    for _ in range(2):
+        new_space = []
+        for i in range(space.shape[0]):
+            if np.all(space[i] == 0):
+                new_space.append(space[i])
+            new_space.append(space[i])
+        space = np.array(new_space).T
+    return space
+
+
 if __name__ == '__main__':
     with open('11/input.txt') as f:
-        lines = np.array([l.strip() for l in f.readlines()])
+        lines = np.array([[c for c in l.strip()] for l in f.readlines()])
     
-    print(lines)
-    m = np.where(lines == '#', 1, 0)
-    print(m)
+    # 1s are gallaxies and 0s are empty space
+    space = np.where(lines == '#', 1, 0)
+    space = fill_empty_space(space)
+    galaxy_coordinates = [c for c in zip(np.nonzero(space))]
+
+    
